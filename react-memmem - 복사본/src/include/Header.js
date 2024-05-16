02e29e2a9b6ad238b2d47aa7cd5a76759d311215
n96js <n96js@naver.com>
2024-05-16 15:50:06 +0900

    복사

--- react-memmem - 복사본/src/include/Header.js
+++ react-memmem - 복사본/src/include/Header.js
@@ -3,19 +3,14 @@ const Header = () => {
     <>
       <div className="header">
         <div>
-          <img
-            src="/images/bug.png"
-            width="30px"
-            height="30px"
-            alt=""
-          />
+          <img src="/images/bug.png" width="30px" height="30px" alt="" />
           <a href="/">
             <h3>Mem</h3>
           </a>
         </div>
         <nav className="nav">
-          <a href="/login">Log in</a>
-          <a href="/join" className="signup_btn">
+          <a href="/main/login">Log in</a>
+          <a href="/main/join" className="signup_btn">
             Sign up
           </a>
         </nav>

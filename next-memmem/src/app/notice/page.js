d76bba864620adf14baff070edf2eeea5ff97995
yangtory <wjdduscldrn@naver.com>
2024-05-24 10:39:05 +0900

    noticeupdate

--- next-memmem/src/app/notice/page.js
+++ next-memmem/src/app/notice/page.js
@@ -1,192 +1,247 @@
-'use client';
-import { useState, useEffect } from 'react';
-import '../../css/table.css';
-import '../../css/notice.css';
-import '../../css/search.css';
-import '../../css/notice_detail.css';
-import { useTicket } from '../../provider/TicketProvider';
-import { useUser } from '../../provider/UserProvider';
-import { useModal } from '../../provider/ModalProvider';
-import { findNotice, selectAll } from '../api/notice';
+"use client";
+import { useState, useEffect } from "react";
+import "../../css/table.css";
+import "../../css/notice.css";
+import "../../css/search.css";
+import "../../css/notice_detail.css";
+import { useTicket } from "../../provider/TicketProvider";
+import { useModal } from "../../provider/ModalProvider";
+import {
+  deleteNotice,
+  findNotice,
+  selectAll,
+  updateNotice,
+} from "../api/notice";
+import { getSession } from "next-auth/react";
 
 const NoticePage = () => {
-    const { noticeList, setNoticeList } = useTicket();
-    const { ccode } = useUser();
-    const { setNoticeModal } = useModal();
-    const [selectedNotice, setSelectedNotice] = useState(null);
-    const [ntitle, setNtitle] = useState('');
-    const [ndate, setNdate] = useState('');
+  const { noticeList, setNoticeList } = useTicket();
+  const { setNoticeModal } = useModal();
+  const [selectedNotice, setSelectedNotice] = useState(null);
+  const [ntitle, setNtitle] = useState("");
+  const [ndate, setNdate] = useState("");
 
-    // 검색 기능
-    const onNtitleChange = (e) => {
-        setNtitle(e.target.value);
+  // 검색 기능
+  const onNtitleChange = (e) => {
+    setNtitle(e.target.value);
+  };
+
+  const onNdateChange = (e) => {
+    setNdate(e.target.value);
+  };
+  const debounce = (callback, delay = 200) => {
+    let debounceTimer;
+    return (...args) => {
+      clearTimeout(debounceTimer);
+      debounceTimer = setTimeout(
+        () => callback.apply(this, args),
+        delay
+      );
     };
+  };
+  const onNtitleHandler = debounce(onNtitleChange, 300);
+  const onNdateHandler = debounce(onNdateChange, 300);
 
-    const onNdateChange = (e) => {
-        setNdate(e.target.value);
+  // insert 클릭하면 modal 띄우기
+  const openModal = () => {
+    setNoticeModal(true);
+  };
+
+  useEffect(() => {
+    const fetchNotices = async () => {
+      const session = await getSession();
+      const ccode = session?.user.id.tbl_company[0].c_code;
+      // 프리즈마를 사용하여 공지사항 데이터 가져오기
+      try {
+        const result = await selectAll({ ccode });
+        setNoticeList([...result]);
+      } catch (error) {
+        console.error("Failed to fetch notices:", error);
+      }
+      const result = await findNotice({
+        ntitle,
+        ndate,
+        ccode,
+      });
+      if (result) {
+        setNoticeList([...result]);
+      }
     };
-    const debounce = (callback, delay = 200) => {
-        let debounceTimer;
-        return (...args) => {
-            clearTimeout(debounceTimer);
-            debounceTimer = setTimeout(() => callback.apply(this, args), delay);
-        };
-    };
-    const onNtitleHandler = debounce(onNtitleChange, 300);
-    const onNdateHandler = debounce(onNdateChange, 300);
+    fetchNotices();
+  }, [setNoticeList, ntitle, ndate, selectedNotice]);
 
-    // insert 클릭하면 modal 띄우기
-    const openModal = () => {
-        setNoticeModal(true);
-    };
+  const handleNoticeClick = (notice) => {
+    setSelectedNotice(notice);
+  };
 
-    useEffect(() => {
-        const fetchNotices = async () => {
-            try {
-                // 프리즈마를 사용하여 공지사항 데이터 가져오기
-                const result = await selectAll({ ccode });
-                setNoticeList([...result]);
-            } catch (error) {
-                console.error('Failed to fetch notices:', error);
-            }
-            const result = await findNotice({
-                ntitle,
-                ndate,
-                ccode,
-            });
-            if (result) {
-                setNoticeList([...result]);
-            }
-        };
-        fetchNotices();
-    }, [setNoticeList, ntitle, ndate]);
+  const handleCloseModal = () => {
+    setSelectedNotice(null);
+  };
 
-    const handleNoticeClick = (notice) => {
-        setSelectedNotice(notice);
-    };
+  const deleteHandler = async (seq) => {
+    await deleteNotice(seq);
+    setSelectedNotice(null);
+  };
 
-    const handleCloseModal = () => {
-        setSelectedNotice(null);
-    };
+  const updateClickHandler = (seq) => {
+    // 클릭-모달-인풋 리드온리지워짐-수정완료버튼생성
+    setSelectedNotice(seq);
+  };
 
-    return (
-        <>
-            <h1 className="list_title">공지사항</h1>
-            <div className="list_home">
-                <div className="insert_btn_box">
-                    <button className="insert button-32" type="button" onClick={openModal}>
-                        글쓰기
-                    </button>
-                </div>
+  const updateHandler = async ({ seq, title, content }) => {
+    await updateNotice({
+      seq,
+      title,
+      content,
+    });
+    setSelectedNotice(null);
+  };
+  return (
+    <>
+      <h1 className="list_title">공지사항</h1>
+      <div className="list_home">
+        <div className="insert_btn_box">
+          <button
+            className="insert button-32"
+            type="button"
+            onClick={openModal}
+          >
+            글쓰기
+          </button>
+        </div>
 
-                <div className="notice search">
-                    <form>
-                        <input
-                            className="search_input"
-                            placeholder="제목"
-                            name="n_title"
-                            defaultValue={ntitle}
-                            onChange={onNtitleHandler}
-                        />
-                        <input
-                            className="search_input"
-                            placeholder="등록일"
-                            name="n_date"
-                            value={ndate}
-                            onChange={onNdateHandler}
-                        />
-                    </form>
-                </div>
+        <div className="notice search">
+          <form>
+            <input
+              className="search_input"
+              placeholder="제목"
+              name="n_title"
+              defaultValue={ntitle}
+              onChange={onNtitleHandler}
+            />
+            <input
+              className="search_input"
+              placeholder="등록일"
+              name="n_date"
+              value={ndate}
+              onChange={onNdateHandler}
+            />
+          </form>
+        </div>
 
-                {noticeList.length === 0 ? (
-                    <div>
-                        <table className="notice list">
-                            <thead>
-                                <tr>
-                                    <th className="index" width="20px">
-                                        No.
-                                    </th>
-                                    <th className="date" width="80px">
-                                        작성일자
-                                    </th>
-                                    <th className="title" width="200px">
-                                        제목
-                                    </th>
-                                    <th className="id" width="20px">
-                                        작성자
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody></tbody>
-                        </table>
-                        <div className="noList_wrapper">
-                            <div className="noList">등록된 공지사항이 없습니다.</div>
-                        </div>
-                    </div>
-                ) : (
-                    <div className="table_div">
-                        <table className="notice list">
-                            <thead>
-                                <tr>
-                                    <th className="index" width="20px">
-                                        No.
-                                    </th>
-                                    <th className="date" width="80px">
-                                        작성일자
-                                    </th>
-                                    <th className="title" width="300x">
-                                        제목
-                                    </th>
-                                    <th className="id" width="20px">
-                                        작성자
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {noticeList.map((notice, index) => (
-                                    <tr key={notice.n_seq} onClick={() => handleNoticeClick(notice)}>
-                                        <td className="index">{index + 1}</td>
-                                        <td className="date">
-                                            {notice.n_date}
-                                            <br />
-                                            {notice.n_time}
-                                        </td>
-                                        <td className="title">{notice.n_title}</td>
-                                        <td className="id">{notice.n_uid}</td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                )}
-                <div className={selectedNotice ? 'modal-backdrop show' : 'modal-backdrop'}>
-                    <div className={selectedNotice ? 'detail show' : 'detail'}>
-                        <div className="close_btn" onClick={handleCloseModal}>
-                            <span>X</span>
-                        </div>
-                        <div className="notice input_box">
-                            <label htmlFor="title">제목</label>
-                            <input id="title" name="title" value={selectedNotice?.n_title} readOnly />
-
-                            <label htmlFor="content">내용</label>
-                            <textarea
-                                id="content"
-                                name="content"
-                                rows="20"
-                                value={selectedNotice?.n_content}
-                                readOnly
-                            ></textarea>
-
-                            <div className="btn_box">
-                                <button className="notice_update button-32">수정</button>
-                                <button className="notice_delete button-32">삭제</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        {noticeList.length === 0 ? (
+          <div>
+            <table className="notice list">
+              <thead>
+                <tr>
+                  <th className="index" width="20px">
+                    No.
+                  </th>
+                  <th className="date" width="80px">
+                    작성일자
+                  </th>
+                  <th className="title" width="200px">
+                    제목
+                  </th>
+                  <th className="id" width="20px">
+                    작성자
+                  </th>
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </table>
+            <div className="noList_wrapper">
+              <div className="noList">
+                등록된 공지사항이 없습니다.
+              </div>
             </div>
-        </>
-    );
+          </div>
+        ) : (
+          <div className="table_div">
+            <table className="notice list">
+              <thead>
+                <tr>
+                  <th className="index" width="20px">
+                    No.
+                  </th>
+                  <th className="date" width="80px">
+                    작성일자
+                  </th>
+                  <th className="title" width="200px">
+                    제목
+                  </th>
+                  <th className="id" width="20px">
+                    작성자
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {noticeList.map((notice, index) => (
+                  <tr
+                    key={notice.n_seq}
+                    onClick={() => handleNoticeClick(notice)}
+                  >
+                    <td className="index">{index + 1}</td>
+                    <td className="date">
+                      {notice.n_date}
+                      <br />
+                      {notice.n_time}
+                    </td>
+                    <td className="title">{notice.n_title}</td>
+                    <td className="id">{notice.n_uid}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+        <div
+          className={
+            selectedNotice ? "modal-backdrop show" : "modal-backdrop"
+          }
+        >
+          <div className={selectedNotice ? "detail show" : "detail"}>
+            <div className="close_btn" onClick={handleCloseModal}>
+              <span>X</span>
+            </div>
+            <div className="notice input_box">
+              <label htmlFor="title">제목</label>
+              <input
+                id="title"
+                name="title"
+                value={selectedNotice?.n_title}
+                readOnly
+              />
+
+              <label htmlFor="content">내용</label>
+              <textarea
+                id="content"
+                name="content"
+                rows="20"
+                value={selectedNotice?.n_content}
+                readOnly
+              ></textarea>
+
+              <div className="btn_box">
+                <button
+                  className="notice_update button-32"
+                  onClick={handleNoticeClick(notice)}
+                >
+                  수정
+                </button>
+                <button
+                  className="notice_delete button-32"
+                  onClick={() => deleteHandler(selectedNotice?.n_seq)}
+                >
+                  삭제
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NoticePage;
--- next-memmem/src/app/teacher/page.js
+++ next-memmem/src/app/teacher/page.js
@@ -1,162 +1,201 @@
-'use client';
-import { useState, useEffect } from 'react';
-import '../../css/table.css'; // CSS 파일을 가져옵니다.
-import '../../css/search.css';
-import { findTeacher, getTeacherInfo, selectAll } from '../api/teacher';
-import { useUser } from '../../provider/UserProvider';
-import { useModal } from '../../provider/ModalProvider';
-import { useTicket } from '../../provider/TicketProvider';
+"use client";
+import { useState, useEffect } from "react";
+import "../../css/table.css"; // CSS 파일을 가져옵니다.
+import "../../css/search.css";
+import "../../css/teacher_detail.css";
+import {
+  findTeacher,
+  getTeacherInfo,
+  selectAll,
+} from "../api/teacher";
+import { useModal } from "../../provider/ModalProvider";
+import { useTicket } from "../../provider/TicketProvider";
+import { getSession } from "next-auth/react";
 const TeacherPage = () => {
-    const { teacherList, setTeacherList } = useTicket();
-    const { ccode } = useUser();
-    const { setTeacherModal } = useModal();
-    const [tname, setTname] = useState('');
-    const [tcode, setTcode] = useState('');
-    const [ttel, setTtel] = useState('');
-    const [detail, setDetail] = useState('');
+  const { teacherList, setTeacherList } = useTicket();
+  const { setTeacherModal } = useModal();
+  const [tname, setTname] = useState("");
+  const [tcode, setTcode] = useState("");
+  const [ttel, setTtel] = useState("");
+  const [detail, setDetail] = useState("");
 
-    // 검색 기능
-    const onTnameChange = (e) => {
-        setTname(e.target.value);
+  // 검색 기능
+  const onTnameChange = (e) => {
+    setTname(e.target.value);
+  };
+
+  const onTcodeChange = (e) => {
+    setTcode(e.target.value);
+  };
+
+  const onTtelChange = (e) => {
+    setTtel(e.target.value);
+  };
+
+  const debounce = (callback, delay = 200) => {
+    let debounceTimer;
+    return (...args) => {
+      clearTimeout(debounceTimer);
+      debounceTimer = setTimeout(
+        () => callback.apply(this, args),
+        delay
+      );
     };
+  };
+  const onTnameHandler = debounce(onTnameChange, 300);
+  const onTcodeHandler = debounce(onTcodeChange, 300);
+  const onTtelHandler = debounce(onTtelChange, 300);
 
-    const onTcodeChange = (e) => {
-        setTcode(e.target.value);
+  // insert 클릭하면 modal 띄우기
+  const openModal = () => {
+    setTeacherModal(true);
+  };
+  // 회원권 리스트 셋팅
+  useEffect(() => {
+    const teacherFetch = async () => {
+      const session = await getSession();
+      const ccode = session?.user.id.tbl_company[0].c_code;
+      try {
+        const result = await selectAll(ccode);
+        console.log(result);
+        setTeacherList([...result]);
+      } catch (error) {
+        console.log(error);
+      }
+      // 검색 기능
+      const result = await findTeacher({
+        tname,
+        tcode,
+        ttel,
+        ccode,
+      });
+      if (result) {
+        setTeacherList([...result]);
+      }
     };
+    teacherFetch();
+  }, [setTeacherList, tcode, tname, ttel]);
 
-    const onTtelChange = (e) => {
-        setTtel(e.target.value);
-    };
+  const teacherViewList = teacherList.map((teacher, index) => (
+    <tr
+      key={teacher?.t_code}
+      onClick={() => setDetail(teacher?.t_code)}
+    >
+      <td>{index + 1}</td>
+      <td>{teacher.t_code}</td>
+      <td>{teacher.t_name}</td>
+      <td>{teacher.t_tel}</td>
+    </tr>
+  ));
 
-    const debounce = (callback, delay = 200) => {
-        let debounceTimer;
-        return (...args) => {
-            clearTimeout(debounceTimer);
-            debounceTimer = setTimeout(() => callback.apply(this, args), delay);
-        };
-    };
-    const onTnameHandler = debounce(onTnameChange, 300);
-    const onTcodeHandler = debounce(onTcodeChange, 300);
-    const onTtelHandler = debounce(onTtelChange, 300);
-
-    // insert 클릭하면 modal 띄우기
-    const openModal = () => {
-        setTeacherModal(true);
-    };
-    // 회원권 리스트 셋팅
-    useEffect(() => {
-        const teacherFetch = async () => {
-            try {
-                const result = await selectAll(ccode);
-                console.log(result);
-                setTeacherList([...result]);
-            } catch (error) {
-                console.log(error);
-            }
-            // 검색 기능
-            const result = await findTeacher({
-                tname,
-                tcode,
-                ttel,
-                ccode,
-            });
-            if (result) {
-                setTeacherList([...result]);
-            }
-        };
-        teacherFetch();
-    }, [setTeacherList, tcode, tname, ttel]);
-
-    const teacherViewList = teacherList.map((teacher, index) => (
-        <tr key={teacher?.t_code} onClick={() => setDetail(teacher?.t_code)}>
-            <td>{index + 1}</td>
-            <td>{teacher.t_code}</td>
-            <td>{teacher.t_name}</td>
-            <td>{teacher.t_tel}</td>
-        </tr>
-    ));
-
-    // detail 데이터 셋팅
-    useEffect(() => {
-        if (detail) {
-            const detailTeacher = async () => {
-                try {
-                    const result = await getTeacherInfo(detail);
-                    setDetail(result[0]);
-                } catch (error) {
-                    console.log(error);
-                }
-            };
-            detailTeacher();
+  // detail 데이터 셋팅
+  useEffect(() => {
+    if (detail) {
+      const detailTeacher = async () => {
+        try {
+          const result = await getTeacherInfo(detail);
+          setDetail(result[0]);
+        } catch (error) {
+          console.log(error);
         }
-    }, [detail, teacherList]);
+      };
+      detailTeacher();
+    }
+  }, [detail, teacherList]);
 
-    return (
-        <>
-            <h1 className="list_title">강사 리스트</h1>
-            <div className="list_home">
-                <div className="teacher input insert_btn_box">
-                    <button className="teacher input insert button-32" type="button" onClick={openModal}>
-                        강사추가
-                    </button>
+  return (
+    <>
+      <h1 className="list_title">강사 리스트</h1>
+      <div className="list_home">
+        <div className="teacher input insert_btn_box">
+          <button
+            className="teacher input insert button-32"
+            type="button"
+            onClick={openModal}
+          >
+            강사추가
+          </button>
+        </div>
+        <div className="teacher btn_box search">
+          <form>
+            <input
+              className="search_input"
+              placeholder="이름"
+              name="tname"
+              defaultValue={tname}
+              onChange={onTnameHandler}
+            />
+            <input
+              className="search_input"
+              placeholder="강사코드"
+              name="tcode"
+              defaultValue={tcode}
+              onChange={onTcodeHandler}
+            />
+            <input
+              className="search_input"
+              placeholder="전화번호"
+              name="ttel"
+              defaultValue={ttel}
+              onChange={onTtelHandler}
+            />
+          </form>
+        </div>
+        <div className="view_box">
+          {detail ? (
+            <div>
+              <div className="detail_box">
+                <div className="card">
+                  <div className="img_box">
+                    <div className="pro_box">
+                      <span className="pro">PRO</span>
+                      <h3>{detail.t_name}</h3>
+                    </div>
+                    {/* <img src="/images/bug.png" width="150px" /> */}
+                  </div>
+                  <div className="teacher_info">
+                    <h4>광주</h4>
+                    <p>
+                      체력단련, 근력강화, 다이어트, 신체균형 <br />
+                      여러 헬스 클럽에서 헬스 트레이너로 근무한 경험이
+                      있음
+                    </p>
+                    <div>
+                      <strong>강사코드</strong>
+                      <span>{detail.t_code}</span>
+                    </div>
+                    <div>
+                      <strong>전화번호</strong>
+                      <span>{detail.t_tel}</span>
+                    </div>
+                  </div>
+                  <div className="detail btn_box">
+                    <button className="button-32">수정</button>
+                    <button className="button-32">삭제</button>
+                  </div>
                 </div>
-                <div className="teacher btn_box search">
-                    <form>
-                        <input
-                            className="search_input"
-                            placeholder="이름"
-                            name="tname"
-                            defaultValue={tname}
-                            onChange={onTnameHandler}
-                        />
-                        <input
-                            className="search_input"
-                            placeholder="강사코드"
-                            name="tcode"
-                            defaultValue={tcode}
-                            onChange={onTcodeHandler}
-                        />
-                        <input
-                            className="search_input"
-                            placeholder="전화번호"
-                            name="ttel"
-                            defaultValue={ttel}
-                            onChange={onTtelHandler}
-                        />
-                    </form>
-                </div>
-                <div className="table_div">
-                    <table className="teacher list">
-                        <thead>
-                            <tr>
-                                <th>No.</th>
-                                <th>강사코드</th>
-                                <th>강사이름</th>
-                                <th>전화번호</th>
-                            </tr>
-                        </thead>
-                        <tbody>{teacherViewList}</tbody>
-                    </table>
-                </div>
-                <div className="view_box">
-                    {detail ? (
-                        <div>
-                            <h1>회원권Detail</h1>
-                            <p>회원권 : {detail.t_code}</p>
-                            <p>횟수 : {detail.t_name}</p>
-                            <p>가격 : {detail.t_tel}</p>
-                            <div className="btn_box">
-                                <button className="update_btn">수정</button>
-                                <button className="delete_btn">삭제</button>
-                            </div>
-                        </div>
-                    ) : (
-                        <p>No ticket selected</p>
-                    )}
-                </div>
+              </div>
             </div>
-        </>
-    );
+          ) : (
+            <div className="nocard">No ticket selected</div>
+          )}
+        </div>
+        <div className="table_div">
+          <table className="teacher list">
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>강사코드</th>
+                <th>강사이름</th>
+                <th>전화번호</th>
+              </tr>
+            </thead>
+            <tbody>{teacherViewList}</tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TeacherPage;
